c7b16c765ee2dc7fa1d718b49317e26307172345
lxsca00 <ljimena10@gmail.com>
2022-05-21 00:37:18 -0500

    arreglo 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import validator from './validator.js';
 
+let creditCardNumber = '5362590285774569'
+
+
 //function seeCard(){
     //document.getElementById('cardVerification').style.display = 'block'  
 //}
