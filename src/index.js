d3ca68279beb175ad14a8341dd9c9b296c1155dc
lxsca00 <ljimena10@gmail.com>
2022-05-23 00:07:46 -0500

    js pt 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import validator from './validator.js';
 
-let creditCardNumber = '5362590285774569'
-
+let creditCard = {
+    user: 'Fulanito Perez',
+    creditCardNumber: 5362590285774569,
+    cvv: 257,
+    dueDate: 04/2024
+}
 
 document.getElementById('go').addEventListener('click', function(event){
     event.preventDefault()
