4ea124a778d95b61eaea248bd15b3800ac0f38c2
lxsca00 <ljimena10@gmail.com>
2022-05-27 16:42:26 -0500

    Función enmascarar

--- src/index.js
+++ src/index.js
@@ -6,37 +6,19 @@ document.getElementById('go').addEventListener('click', function(){
   })
 
 
-document.getElementById('verificar').addEventListener('click', function(){
-    let cardNumber = document.getElementById('card').value; //Paso 1: Toma el número de tarjeta desde el input text
-    let firstNumbers = cardNumber.split(''); //Paso 2: Separa el número el un array de strings
-    let digits = firstNumbers.reverse(); // Paso 3: Invierte el orden del array, continuan siendo strings
-    let cardPairNumbers = 0; //(Esta variable almacena la suma de los números de las posiciones pares luego de sumar los digitos de los >=10)
-    let cardOodNumbers = 0; //(Esta variable almacena la suma de los números de las posiciones impares)
-    
-    console.log(digits)
-    for (let i= 1; i<digits.length;i=i+2){
-        let doubleDigits = digits[i]*2 // Paso 4: Devuelve la multiplicación de números pares
-        let pairNumbers
-          if (doubleDigits>= 10){ //Paso 5: Suma los digitos de los números mayores a 10
-            pairNumbers = doubleDigits.toString().split('').reduce(function(r,n){return parseInt(r)+parseInt(n)})
-          } else {pairNumbers = doubleDigits}
-        cardPairNumbers += pairNumbers //Paso 6: Suma de los números pares
-      }
-    console.log(cardPairNumbers)
-    for (let i=0;i<digits.length;i=i+2){ 
-        cardOodNumbers += parseInt(digits[i]) //Paso 7: Suma de los números impares
-    }
-    let ifItsValid = (cardOodNumbers + cardPairNumbers)%10
-    console.log(cardOodNumbers)
-    console.log(ifItsValid)
-    if(ifItsValid===0){
+
+
+document.getElementById('verificar').addEventListener('click', function(){ 
+  let creditCardNumber = document.getElementById('card').value;
+  //console.log(creditCardNumber)
+    if(validator.isValid(creditCardNumber)===true){
       document.getElementById('datos').innerHTML='Tarjeta válida'
       document.getElementById('card').style.backgroundColor = '#E4F6BD'
     } else {
-      document.getElementById('datos').innerHTML='Tarjeta inválida'
+      document.getElementById('datos').innerHTML='Tajeta inválida'
       document.getElementById('card').style.backgroundColor = '#F9D0CD'
     }
-    
+   document.getElementById('demo').innerHTML= validator.maskify(creditCardNumber) 
   })   
   
       
--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,38 @@
 const validator = {
   isValid: function (creditCardNumber){
-  console.log("201")}
+  let cardNumber = document.getElementById('card').value.split('');//Paso 1: Toma el número de tarjeta desde el input text y lo separa
+  let digits = cardNumber.reverse(); // Paso 3: Invierte el orden del array, continuan siendo strings
+  let cardPairNumbers = 0; //(Esta variable almacena la suma de los números de las posiciones pares luego de sumar los digitos de los >=10)
+  let cardOodNumbers = 0; //(Esta variable almacena la suma de los números de las posiciones impares)
+    
+    //console.log(digits)
+    for (let i= 1; i<digits.length;i=i+2){
+        let doubleDigits = digits[i]*2 // Paso 4: Devuelve la multiplicación de números pares
+        let pairNumbers
+          if (doubleDigits>= 10){ //Paso 5: Suma los digitos de los números mayores a 10
+            pairNumbers = doubleDigits.toString().split('').reduce(function(r,n){return parseInt(r)+parseInt(n)})
+          } else {pairNumbers = doubleDigits}
+        cardPairNumbers += pairNumbers //Paso 6: Suma de los números pares
+      }
+    //console.log(cardPairNumbers)
+    for (let i=0;i<digits.length;i=i+2){ 
+        cardOodNumbers += parseInt(digits[i]) //Paso 7: Suma de los números impares
+    }
+    let ifItsValid = (cardOodNumbers + cardPairNumbers)%10
+    //console.log(cardOodNumbers)
+   // console.log(ifItsValid)
+    if (ifItsValid===0){
+      return true
+    }
+  },
 
-
-  //maskify: function (creditCardNumber){}
+  maskify: function(creditCardNumber){
+    creditCardNumber = document.getElementById('card').value
+    function maskNumbers (num,mask){
+      return (num).slice(0,-4).replace(/./g,mask)+(num).slice(-4)
+    }
+    return maskNumbers(creditCardNumber,'*')
+  }
 }
 
 
