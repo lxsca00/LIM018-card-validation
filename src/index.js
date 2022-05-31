e05ca709f1ad4eaa38f8569aa8c60eb092b72ee8
lxsca00 <ljimena10@gmail.com>
2022-05-31 00:03:32 -0500

    Nuevo maskify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import validator from './validator.js';
 document.getElementById('go').addEventListener('click', function(){
     document.getElementById('cardVerification').style.display='block'
     document.getElementById('ready').style.display='none'
+    document.getElementById('brand').style.filter= 'blur(2px)'
   })
 
 
@@ -10,16 +11,22 @@ document.getElementById('go').addEventListener('click', function(){
 
 document.getElementById('verificar').addEventListener('click', function(){ 
   let creditCardNumber = document.getElementById('card').value;
+  if (creditCardNumber==''){
+    return 
+  } 
   //console.log(creditCardNumber)
     if(validator.isValid(creditCardNumber)===true){
-      document.getElementById('datos').innerHTML='Tarjeta válida'
+      document.getElementById('datos').innerHTML='¡Tarjeta validada!'
       document.getElementById('card').style.backgroundColor = '#E4F6BD'
     } else {
-      document.getElementById('datos').innerHTML='Tajeta inválida'
+      document.getElementById('datos').innerHTML='Tarjeta inválida...'
       document.getElementById('card').style.backgroundColor = '#F9D0CD'
     }
-   document.getElementById('demo').innerHTML= validator.maskify(creditCardNumber) 
+  //console.log(validator.maskify(creditCardNumber))
+  
+  document.getElementById('card').value=validator.maskify(creditCardNumber)
   })   
+
   
       
-console.log(validator);
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,10 @@
 const validator = {
   isValid: function (creditCardNumber){
-  let cardNumber = document.getElementById('card').value.split('');//Paso 1: Toma el número de tarjeta desde el input text y lo separa
+  let cardNumber = creditCardNumber.split('');//Paso 1: Toma el número de tarjeta y lo separa
   let digits = cardNumber.reverse(); // Paso 3: Invierte el orden del array, continuan siendo strings
   let cardPairNumbers = 0; //(Esta variable almacena la suma de los números de las posiciones pares luego de sumar los digitos de los >=10)
   let cardOodNumbers = 0; //(Esta variable almacena la suma de los números de las posiciones impares)
-    
-    //console.log(digits)
+
     for (let i= 1; i<digits.length;i=i+2){
         let doubleDigits = digits[i]*2 // Paso 4: Devuelve la multiplicación de números pares
         let pairNumbers
@@ -14,24 +13,26 @@ const validator = {
           } else {pairNumbers = doubleDigits}
         cardPairNumbers += pairNumbers //Paso 6: Suma de los números pares
       }
-    //console.log(cardPairNumbers)
+
     for (let i=0;i<digits.length;i=i+2){ 
         cardOodNumbers += parseInt(digits[i]) //Paso 7: Suma de los números impares
     }
     let ifItsValid = (cardOodNumbers + cardPairNumbers)%10
-    //console.log(cardOodNumbers)
-   // console.log(ifItsValid)
     if (ifItsValid===0){
       return true
+    } else {
+      return false
     }
   },
 
-  maskify: function(creditCardNumber){
-    creditCardNumber = document.getElementById('card').value
-    function maskNumbers (num,mask){
-      return (num).slice(0,-4).replace(/./g,mask)+(num).slice(-4)
+  maskify: function hideNumbers (creditCardNumber){
+    let toMask = creditCardNumber.split(''); //Paso 1: Toma el número de tarjeta y lo separa
+    let hideNumbers =''
+    for (let i=0;i<toMask.length-4;i++){
+      hideNumbers += (toMask[i]='#') //Paso 2: Toma los números del array excepto los 4 últimos y los convierte a #
     }
-    return maskNumbers(creditCardNumber,'*')
+    let visibleNumbers = creditCardNumber.slice(-4) //Paso 3: Toma los últimos 4 números
+    return hideNumbers + visibleNumbers
   }
 }
 
