dfdd9d309ed47bb31879da655c1631d437400ca3
lxsca00 <ljimena10@gmail.com>
2022-06-02 11:59:56 -0500

    try readme 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import validator from './validator.js'; 
 
 document.getElementById('goPay').addEventListener('click', function(){
-    document.getElementById('cardVerification').style.display='block';
+    document.getElementById('payPage').style.display='block';
     document.getElementById('ready').style.display='none';
     document.getElementById('brand').style.filter= 'blur(2px)';
     document.querySelector('.myShoppingCart').style.display='none';
@@ -9,7 +9,7 @@ document.getElementById('goPay').addEventListener('click', function(){
 
 document.getElementById('exit').addEventListener('click', function(){
   if (document.getElementById('card').style.display='block'){
-    document.getElementById('cardVerification').style.display='none';
+    document.getElementById('payPage').style.display='none';
     document.getElementById('ready').style.display='block';
     document.getElementById('brand').style.filter='none';
     document.querySelector('.myShoppingCart').style.display='block'
@@ -26,11 +26,9 @@ document.getElementById('verificar').addEventListener('click', function(){
     if(validator.isValid(creditCardNumber)===true){
       document.getElementById('datos').innerHTML='¡Tarjeta validada!'
       document.getElementById('card').style.backgroundColor = '#E4F6BD'
-      document.getElementById('tryAgain').style.visibility='hidden'
     } else {
       document.getElementById('datos').innerHTML='Tu tarjeta no es válida...'
       document.getElementById('card').style.backgroundColor = '#F9D0CD'
-      document.getElementById('tryAgain').style.visibility='visible'
     }
   
   document.getElementById('card').value=validator.maskify(creditCardNumber)
