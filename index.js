c3bf4cd1654084731ca283a2558ba58df7751ed2
lxsca00 <ljimena10@gmail.com>
2022-06-02 11:36:22 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,35 @@
+import validator from './validator.js'; 
+
+document.getElementById('goPay').addEventListener('click', function(){
+    document.getElementById('payPage').style.display='block';
+    document.getElementById('ready').style.display='none';
+    document.getElementById('brand').style.filter= 'blur(2px)';
+    document.querySelector('.myShoppingCart').style.display='none';
+  })
+
+document.getElementById('exit').addEventListener('click', function(){
+  if (document.getElementById('card').style.display='block'){
+    document.getElementById('payPage').style.display='none';
+    document.getElementById('ready').style.display='block';
+    document.getElementById('brand').style.filter='none';
+    document.querySelector('.myShoppingCart').style.display='block'
+  }
+})
+
+
+document.getElementById('verificar').addEventListener('click', function(){ 
+  let creditCardNumber = document.getElementById('card').value;
+  if (creditCardNumber==''){
+    return 
+  } 
+
+    if(validator.isValid(creditCardNumber)===true){
+      document.getElementById('datos').innerHTML='¡Tarjeta validada!'
+      document.getElementById('card').style.backgroundColor = '#E4F6BD'
+    } else {
+      document.getElementById('datos').innerHTML='Tu tarjeta no es válida...'
+      document.getElementById('card').style.backgroundColor = '#F9D0CD'
+    }
+  
+  document.getElementById('card').value=validator.maskify(creditCardNumber)
+  })   
---
+++ validator.js
@@ -0,0 +1,41 @@
+const validator = {
+  isValid: function (creditCardNumber){
+  let cardNumber = creditCardNumber.split('');//Paso 1: Toma el número de tarjeta y lo separa
+  let digits = cardNumber.reverse(); // Paso 3: Invierte el orden del array, continuan siendo strings
+  let cardPairNumbers = 0; //(Esta variable almacena la suma de los números de las posiciones pares luego de sumar los digitos de los >=10)
+  let cardOodNumbers = 0; //(Esta variable almacena la suma de los números de las posiciones impares)
+
+    for (let i= 1; i<digits.length;i=i+2){
+        let doubleDigits = digits[i]*2 // Paso 4: Devuelve la multiplicación de números pares
+        let pairNumbers
+          if (doubleDigits>= 10){ //Paso 5: Suma los digitos de los números mayores a 10
+            pairNumbers = doubleDigits.toString().split('').reduce(function(r,n){return parseInt(r)+parseInt(n)})
+          } else {pairNumbers = doubleDigits}
+        cardPairNumbers += pairNumbers //Paso 6: Suma de los números pares
+      }
+
+    for (let i=0;i<digits.length;i=i+2){ 
+        cardOodNumbers += parseInt(digits[i]) //Paso 7: Suma de los números impares
+    }
+    let ifItsValid = (cardOodNumbers + cardPairNumbers)%10
+    if (ifItsValid===0){
+      return true
+    } else {
+      return false
+    }
+  },
+
+  maskify: function hideNumbers (creditCardNumber){
+    let toMask = creditCardNumber.split(''); //Paso 1: Toma el número de tarjeta y lo separa
+    let hideNumbers =''
+    for (let i=0;i<toMask.length-4;i++){
+      hideNumbers += (toMask[i]='#') //Paso 2: Toma los números del array excepto los 4 últimos y los convierte a #
+    }
+    let visibleNumbers = creditCardNumber.slice(-4) //Paso 3: Toma los últimos 4 números
+    return hideNumbers + visibleNumbers
+  }
+}
+
+
+
+export default validator;
