d01b4192d95d6de65f2a52f895563fb89f5b4447
lxsca00 <ljimena10@gmail.com>
2022-06-02 11:57:13 -0500

    try readme

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ document.getElementById('exit').addEventListener('click', function(){
     document.getElementById('cardVerification').style.display='none';
     document.getElementById('ready').style.display='block';
     document.getElementById('brand').style.filter='none';
-    document.querySelector('.myShoppingCart').style.display='block';
+    document.querySelector('.myShoppingCart').style.display='block'
   }
 })
 
@@ -26,9 +26,11 @@ document.getElementById('verificar').addEventListener('click', function(){
     if(validator.isValid(creditCardNumber)===true){
       document.getElementById('datos').innerHTML='¡Tarjeta validada!'
       document.getElementById('card').style.backgroundColor = '#E4F6BD'
+      document.getElementById('tryAgain').style.visibility='hidden'
     } else {
-      document.getElementById('datos').innerHTML='Tarjeta inválida...'
+      document.getElementById('datos').innerHTML='Tu tarjeta no es válida...'
       document.getElementById('card').style.backgroundColor = '#F9D0CD'
+      document.getElementById('tryAgain').style.visibility='visible'
     }
   
   document.getElementById('card').value=validator.maskify(creditCardNumber)
